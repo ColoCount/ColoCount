1800a14577066eee4b0926798d5191730d2c4bb1
Romain Lhuillier <romainlhuillier94@gmail.com>
2023-01-10 13:56:22 +0100

    feat : add user management login

--- Back/app/src/Controller/UserController.php
+++ Back/app/src/Controller/UserController.php
@@ -2,15 +2,45 @@
 
 namespace App\Controller;
 
+use App\Model\Factory\PDO;
 use App\Model\Route\Route;
 use App\Model\Manager\UserManager;
 use App\Service\JWTHelper;
+use App\Base\CookieHelper;
 
 
 class UserController
 {
-    #[Route('/login', name:"login", methods:["Post"])]
+    #[Route('/login', name:"login", methods:["POST"])]
     public function login(){
-        var_dump($_POST);die;
+        if($_SERVER['REQUES_METHOD'] == 'POST'){
+            if(!empty($_POST)){
+                if(isset($_POST['login'],$_POST['password']) && !empty($_POST['login']) && !empty($_POST['password'])){
+                    $login = htmlspecialchars(strip_tags($_POST['login']));
+                    $password = htmlspecialchars(strip_tags($_POST['password']));
+
+                    $connectioPDO = new UserManager(new PDO());
+                    $user = $connectioPDO->login($login,$password);
+
+                    if($user)
+                    $jwt = JWTHelper::buildJWT($user);
+                    
+                    CookieHelper::setCookie($jwt);
+                    
+                    echo json_encode([
+                        'status' => 'success',
+                        "id" => $user->getId(),
+                        'username' => $user->getUsername(),
+                        'token' => $jwt
+                    ]);
+                    exit;
+                }
+            }
+        }
+        echo json_encode([
+            'status' => 'error',
+            'message' => 'Pas de connexion'
+        ]);
+        exit;
     }
 }
--- Back/app/src/Model/Manager/UserManager.php
+++ Back/app/src/Model/Manager/UserManager.php
@@ -7,5 +7,26 @@ use App\Model\Entity\User;
 
 class UserManager extends BaseManager
 {
-    
+
+    public function login($username,$password):?User
+    {
+        $sql = "select `id`,`password`,`email` from `Users` where `username` = :username";
+        
+        $query = $this->pdo->prepare($sql);
+        $query->bindValue(':username', $username,\PDO::PARAM_STR);
+
+        $query->execute();
+
+        $response = $query->fetch();
+
+        if(!$response){
+            return null;
+        }
+
+        if(!password_verify($password,$response["password"])){
+            return null;
+        }
+
+        return  new User($response);
+    }
 }
