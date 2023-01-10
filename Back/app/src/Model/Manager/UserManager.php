fce838a3fc20c34ddd168b9c1b01272a5383dfb3
Romain Lhuillier <romainlhuillier94@gmail.com>
2023-01-10 10:32:00 +0100

    feat : set up base

---
+++ Back/app/src/Model/Manager/UserManager.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model\Manager;
+
+use App\Base\BaseManager;
+use App\Model\Entity\User;
+
+class UserManager extends BaseManager
+{
+    
+}
--- Back/database/coloCount.sql
+++ Back/database/coloCount.sql
@@ -2,13 +2,12 @@
 DROP TABLE IF EXISTS `Users`;
 CREATE TABLE IF NOT EXISTS `Users`(
     `id` integer(11) NOT NULL AUTO_INCREMENT,
-    `lastname` varchar(40),
-    `firstname` varchar(40),
-    `login` varchar(30) NOT NULL UNIQUE,
+    `username` varchar(30) NOT NULL UNIQUE,
+    `email` varchar (255) NOT NULL UNIQUE,
     `password` varchar(60) NOT NULL,
     PRIMARY KEY (`id`)
 );
 
 /* Insertion d'un utilisateur avec le role admin dans la table `User`*/
-INSERT INTO `Users` (`id`, `lastname`, `firstname`,`login`, `password`) VALUES
-('1', 'Admin', 'Admin', 'admin', '$2y$10$OgGilVcpTrARPRsrx8YZf.GRCGW3EAugei7htlwYaGDdbROVRY2pu');
+INSERT INTO `Users` (`id`,`username`, `password`) VALUES
+('1', 'Admin','admin@gmail.com','$2y$10$OgGilVcpTrARPRsrx8YZf.GRCGW3EAugei7htlwYaGDdbROVRY2pu');
