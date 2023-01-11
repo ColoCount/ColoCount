e6411d07f346a82c0df083b44def58b5a5d21101
Romain Lhuillier <romainlhuillier94@gmail.com>
2023-01-11 10:58:39 +0100

    feat : add register

--- Back/app/src/Model/Manager/UserManager.php
+++ Back/app/src/Model/Manager/UserManager.php
@@ -8,12 +8,12 @@ use App\Model\Entity\User;
 class UserManager extends BaseManager
 {
 
-    public function login($userName,$password):?User
+    public function login($username,$password):?User
     {
-        $sql = "select `id`,`password`,`email` from `Users` where `userName` = :userName";
+        $sql = "select `id`,`password`,`email` from `Users` where `username` = :username";
         
         $query = $this->pdo->prepare($sql);
-        $query->bindValue(':userName', $userName,\PDO::PARAM_STR);
+        $query->bindValue(':username', $username,\PDO::PARAM_STR);
 
         $query->execute();
 
@@ -26,19 +26,53 @@ class UserManager extends BaseManager
         if(!password_verify($password,$response["password"])){
             return null;
         }
-
-        return  new User($response);
+        
+        return new User($response);
     }
+
+    public function register($username,$email,$password):User
+    {
+
+        $sql = "INSERT INTO `Users` (`username`,`password`,`email`) VALUES (:username, :password, :email)";
+
+        $query = $this->pdo->prepare($sql);
+
+        $query->bindValue(':username', $username, \PDO::PARAM_STR);
+        $query->bindValue(':password', $password, \PDO::PARAM_STR);
+        $query->bindValue(':email', $email, \PDO::PARAM_STR);
+
+        $query->execute();
+
+        return $this->getUserById($this->pdo->lastInsertId());
+    }
+
+    public function getUserById($id):User
+    {
+        $sql = "select `id`,`email`,`username` from `Users` where `id` = :id";
+
+        $query = $this->pdo->prepare($sql);
+        $query->bindValue(':id', $id, \PDO::PARAM_STR);
+        $query->execute();
+
+        $response = $query->fetch();
+
+        if(!$response){
+            return null;
+        }
+
+        return new User($response);
+    }
+
 
     public function getAllColocUser($idColoc):?array
     {
 
         $sql = "select `idUser` from `ColocUser` where `idColoc` = :idColoc";
-
         $query = $this->pdo->prepare($sql);
         $query->bindValue(':idColoc', $idColoc,\PDO::PARAM_STR);
         $query->execute();
         $response = [];
+        
         while ($data = $query->fetch(\PDO::FETCH_ASSOC)) {
 
             $id = $data['idUser'];
@@ -46,8 +80,8 @@ class UserManager extends BaseManager
 
             $query = $this->pdo->prepare($sql);
             $query->bindValue(':id', $id,\PDO::PARAM_STR);
-
             $query->execute();
+
             $user = $query->fetch();
             $response[] = new User($user);
 
