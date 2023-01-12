3dd6243fff44b4f79c1cb9098c7fddd75286836b
polinzz <paulinemiranda@hotmail.fr>
2023-01-12 14:48:21 +0100

    feat : css login reg

--- Front/client/src/App.js
+++ Front/client/src/App.js
@@ -1,4 +1,8 @@
-import './App.css';
+import './assets/css/reset.css';
+import './assets/css/texts.css';
+import './assets/css/buttons.css';
+import './assets/css/box.css';
+import './assets/css/page-login-register.css';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 
