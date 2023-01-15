e5f86433332b6a9a966b227e9f283ebdf1b045e4
JosephMenard <91746407+JosephMenard@users.noreply.github.com>
2023-01-15 18:49:29 +0100

    Update modalMonProfil.js

--- Front/client/src/modales/modalMonProfil.js
+++ Front/client/src/modales/modalMonProfil.js
@@ -4,36 +4,63 @@ import ProfilPhoto from "../components/ProfilPhoto";
 import '../assets/css/modales/modale-mon-profile.css';
 
 
+
 const MonProfil = () => {
-    const [username, setusername] = useState("");
-    const [password, setpassword] = useState("");
-    const [profilPicture, setProfilPicture] = useState("");
+    const [confirm_password, setConfirm_password] = useState("");
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [old_password, setOld_password] = useState("");
+    const userName = "Joseph";
+    const email = "Josephmenard@gmail.com";
+
+    const [show_profil, setShow_profil] = useState(true);
+    const [show_form, setShow_form] = useState(false);
+    // setShow_form(false);
+
+    const hideAndShow = async (event) => {
+        event.preventDefault();
+        setShow_profil(false);
+        setShow_form(true);
+
+    }
+    const handleSubmit =  async (event) => {
+        event.preventDefault();
 
 
-    const handleUser = (e) => {
-        e.preventDefault();
-        //login({ nameColoc, description }, dispatch);
+        // Récupération du token stocké dans le localStorage
+        const token = localStorage.getItem('token');
+        console.log(token);
+
+        // Création de l'en-tête avec le token d'authentification
+        const headers = {
+            "Content-type":  "application/x-www-form-urlencoded",
+            'Authorization': `Bearer ${token}`
+        };
+
+        // Envoi de la requête pour ajouter une colocation
+        try {
+            const response = await fetch('http://localhost:1501/mes_colocs/mon_compte', {
+                method: 'POST',
+                mode: "cors",
+                credentials: "include",
+                headers,
+                body: new URLSearchParams({ username, old_password, password, confirm_password })
+            });
+            const data = await response.json();
+            console.log(data);
+            setShow_profil(true);
+            setShow_form(false);
+        } catch (error) {
+            console.error(error);
+
+        }
     }
 
-    // // Using Fetch API
-    // useEffect(() => {
-    //     fetch('http://localhost:1501/posts?_limit=5')
-    //         .then((res) => res.json())
-    //         .then((data) => {
-    //             console.log(data);
-    //             //setPosts(data);
-    //         })
-    //         .catch((err) => {
-    //             console.log(err.message);
-    //         });
-
-    // }, []);
-
-     const componentDidMount = () => {
-        fetch('http://localhost:1501/users').then((response) => {
-            console.log(response.json());
-        })
-    }
+    // const componentDidMount = () => {
+    //    fetch('http://localhost:1501/users').then((response) => {
+    //        console.log(response.json());
+    //    })
+    // }
 
 
 
@@ -44,72 +71,47 @@ const MonProfil = () => {
                 <div className="box-model-scroll">
                     <div className="cross-modale"></div>
                     <h2 className="text-center">Mon profil</h2>
-                    <div className="image-profil">
-                        <div className="image-mon-profile">
-                            <ProfilPhoto/>
+                        <div className="profil">
+                            {show_profil ?
+                                // <ProfilPhoto/>
                             <div className="overlay-photo" id="iconModifPseudo">
                                 <div className="icon-modif-profil">
                                     <img src={iconModif}/>
                                 </div>
-                            </div>
+                            </div>:""}
+                            {show_profil ?
+                                // <ProfilPhoto/>
+                                <div className="userName" id="ModifPseudo">
+                                    <h2 className="field-title pseudo">Votre pseudo</h2>
+                                    <p className="field pseudo">{userName}</p>
+                                </div>:""}
+                            {show_profil ?
+                            <div className="email" id="affiche-Email">
+                                <h2 className="field-title email">Votre email</h2>
+                                <p className="field email">{email}</p>
+                            </div>:""}
+                            {show_profil ?
+                                <div className="password" id="affiche-password">
+                                <h2 className="field-title password">Votre password</h2>
+                                <p className="field password">*******</p>
+                            </div>:""}
+                            {show_profil ?
+                            <button onClick={hideAndShow} className="link para-16 medium text-center" >Modifier mon profil</button>
+                                :""}
                         </div>
-                        <form className="form-profil form-image">
-                            <div className="fields-column one-fields profile-picture">
-                                <div className="fields-row">
-                                    <div className="img-wrap img-upload">
-                                        <img htmlFor="photo-upload" src="Front/client/src/modales/modalMonProfil"/>
-                                    </div>
-                                    {/* <input id="photo-upload" type="file" onChange={(e) => setProfilPicture(e.target.value)}/> */}
-                                </div>
+                    {show_form ?
+                        <form className="form-profil">
+                            <div className="fields-row username">
+                                <input type="text" id="username" className="form-control form-control-lg" placeholder="Choisir nouveau pseudo" onChange={(e) => setUsername(e.target.value)}/>
                             </div>
-                            <button type="submit" onClick={handleUser} className="link para-16 medium text-center" >Mettre à jour ma photo</button>
-                        </form>
-                    </div>
-                    <div className="pseudo-profil">
-                        <div className="pseudo-mon-profile">
-                            <h3 className="text-center">Pseudo à afficher</h3>
-                            <div className="icon-modif-profil">
-                                <img src={iconModif}/>
+                            <div className="fields-row password">
+                                <input type="text" id="ancienPassword" className="form-control form-control-lg" placeholder="Renseigner votre ancien mot de passe" onChange={(e) => setOld_password(e.target.value)}/>
+                                <input type="text" id="nouveauPassword" className="form-control form-control-lg" placeholder="Choisir un nouveau mot de passe" onChange={(e) => setPassword(e.target.value)}/>
+                                <input type="text" id="confirmPassword" className="form-control form-control-lg" placeholder="Confirmer votre nouveau  mot de passe" onChange={(e) => setConfirm_password(e.target.value)}/>
                             </div>
-                        </div>
-                        <form className="form-profil form-pseudo">
-                            <div className="fields-row">
-                                <input type="text" id="form3Example3" className="form-control form-control-lg" placeholder="Pseudo à afficher" onChange={(e) => setusername(e.target.value)}/>
-                            </div>
-                            <button type="submit" onClick={handleUser} className="link para-16 medium text-center" >Mettre à jour mon pseudo</button>
-                        </form>
-                    </div>
-                    <div className="email-profil">
-                        <div className="email-mon-profile">
-                            <h3 className="text-center">Mon adresse mail</h3>
-                            <p className="text-center">adresse à afficher</p>
-                        </div>
-                    </div>
-                    <div className="password-profil">
-                        <h3 className="text-center">Mon mot de passe</h3>
-                        <div className="password-mon-profile">
-                            <div className="bloc-icon">
-                                <p className="text-center">******</p>
-                                <div className="icon-modif-profil" id="iconModifPassword">
-                                    <img src={iconModif}/>
-                                </div>
-                            </div>
-                        </div>
-                        <form className="form-profil form-password">
-                            <div className="fields-column tree-fields password">
-                                <div className="fields-row">
-                                    {/* <input type="text" id="ancien" className="form-control form-control-lg" placeholder="Ancien mot de passe" onChange={(e) => setancienPwd(e.target.value)}/> */}
-                                </div>
-                                <div className="fields-row">
-                                    {/* <input type="text" id="nouveau" className="form-control form-control-lg" placeholder="Nouveau mot de passe" onChange={(e) => setnouveauPwd(e.target.value)}/> */}
-                                </div>
-                                <div className="fields-row">
-                                    {/* <input type="text" id="confirmation" className="form-control form-control-lg" placeholder="Confirmation du nouveau mot de passe" onChange={(e) => setconfNouveauPwd(e.target.value)}/> */}
-                                </div>
-                            </div>
-                            {/* <button type="submit" onClick={handleUser} className="link para-16 medium text-center" >Modifier mon mot de passe</button> */}
-                        </form>
-                    </div>
+                            <button type="submit" onClick={handleSubmit} className="link para-16 medium text-center" >Modifier mon profil</button>
+                        </form>:""
+                    }
                 </div>
             </div>
         </div>
